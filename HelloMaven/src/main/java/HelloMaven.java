9610c99307ec07244a373373d4a5ea8d89040881
GudBoyGravy <gsudweeks@student.neumont.edu>
2021-07-06 09:47:39 -0600

    Committing tax fraud

---
+++ HelloMaven/src/main/java/HelloMaven.java
@@ -0,0 +1,14 @@
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		System.out.println("Wazzup Maven?!?!?");
+
+		
+		int x = 20;
+		System.out.println("Wow wow zow");
+		
+		int y = 44;
+	}
+
+}
